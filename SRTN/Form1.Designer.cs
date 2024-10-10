8ffb5c76d9e2b9eee42ebc607919b92bc5134b7a
Your Name <your.email@example.com>
2024-10-10 14:16:46 +0700

    Ok

---
+++ SRTN/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SRTN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dgv_process = new System.Windows.Forms.DataGridView();
+            this.process_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time_arrival = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time_burst = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time_wait = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time_wait_ave = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time_finish = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_process)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số lượng tiến trình";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.comboBox1.Location = new System.Drawing.Point(150, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(182, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(338, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Chọn";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(609, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Chạy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // dgv_process
+            // 
+            this.dgv_process.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_process.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_process.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.process_id,
+            this.time_arrival,
+            this.time_burst,
+            this.time_wait,
+            this.time_wait_ave,
+            this.time_finish});
+            this.dgv_process.Location = new System.Drawing.Point(41, 90);
+            this.dgv_process.Name = "dgv_process";
+            this.dgv_process.Size = new System.Drawing.Size(643, 348);
+            this.dgv_process.TabIndex = 3;
+            // 
+            // process_id
+            // 
+            this.process_id.HeaderText = "Mã tiến trình";
+            this.process_id.Name = "process_id";
+            // 
+            // time_arrival
+            // 
+            this.time_arrival.HeaderText = "Thời gian đến";
+            this.time_arrival.Name = "time_arrival";
+            // 
+            // time_burst
+            // 
+            this.time_burst.HeaderText = "Thời gian thực thi";
+            this.time_burst.Name = "time_burst";
+            // 
+            // time_wait
+            // 
+            this.time_wait.HeaderText = "Thời gian chờ";
+            this.time_wait.Name = "time_wait";
+            // 
+            // time_wait_ave
+            // 
+            this.time_wait_ave.HeaderText = "Thời gian chờ trung bình";
+            this.time_wait_ave.Name = "time_wait_ave";
+            // 
+            // time_finish
+            // 
+            this.time_finish.HeaderText = "Thời gian hoàn thành";
+            this.time_finish.Name = "time_finish";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 450);
+            this.Controls.Add(this.dgv_process);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_process)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dgv_process;
+        private System.Windows.Forms.DataGridViewTextBoxColumn process_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time_arrival;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time_burst;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time_wait;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time_wait_ave;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time_finish;
+    }
+}
+
---
+++ SRTN/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SRTN
+{
+    public partial class Form1 : Form
+    {
+        public class Process
+        {
+            public string ProcessID { get; set; }
+            public int ArrivalTime { get; set; }
+            public int BurstTime { get; set; }
+            public int RemainingTime { get; set; }
+            public int WaitingTime { get; set; }
+            public int CompletionTime { get; set; }
+
+            public Process(string id, int arrival, int burst)
+            {
+                ProcessID = id;
+                ArrivalTime = arrival;
+                BurstTime = burst;
+                RemainingTime = burst;
+                WaitingTime = 0;
+                CompletionTime = 0;
+            }
+        }
+
+        private List<Process> processes = new List<Process>();
+        private Random rand = new Random();
+        private int currentTime = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+        }
+
+        private void InitializeDataGridView()
+        {
+            dgv_process.Columns.Clear();
+            dgv_process.Columns.Add("ProcessID", "Mã tiến trình");
+            dgv_process.Columns.Add("ArrivalTime", "Thời gian đến");
+            dgv_process.Columns.Add("BurstTime", "Thời gian thực thi");
+            dgv_process.Columns.Add("WaitingTime", "Thời gian chờ");
+            dgv_process.Columns.Add("CompletionTime", "Thời gian hoàn thành");
+            dgv_process.AllowUserToAddRows = false;
+
+            // Thiết lập các cột WaitingTime và CompletionTime là chỉ đọc
+            dgv_process.Columns["WaitingTime"].ReadOnly = true;
+            dgv_process.Columns["CompletionTime"].ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Lấy số lượng tiến trình từ ComboBox
+            int numProcesses = Convert.ToInt32(comboBox1.SelectedItem);
+            processes.Clear();
+            dgv_process.Rows.Clear();
+
+            for (int i = 0; i < numProcesses; i++)
+            {
+                string processId = "P" + (i + 1);
+                int arrivalTime = rand.Next(0, 10);
+                int burstTime = rand.Next(1, 10);
+                Process process = new Process(processId, arrivalTime, burstTime);
+                processes.Add(process);
+                dgv_process.Rows.Add(processId, arrivalTime, burstTime, "", ""); // Thêm các ô trống cho thời gian chờ và hoàn thành
+            }
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            // Tính toán SRTN
+            List<Process> completedProcesses = new List<Process>();
+
+            while (processes.Any(p => p.CompletionTime == 0))
+            {
+                // Lọc các tiến trình đã đến
+                var readyProcesses = processes.Where(p => p.ArrivalTime <= currentTime && p.CompletionTime == 0).ToList();
+
+                if (readyProcesses.Count > 0)
+                {
+                    // Chọn tiến trình có thời gian còn lại ngắn nhất
+                    Process currentProcess = readyProcesses.OrderBy(p => p.RemainingTime).First();
+
+                    // Thực hiện một đơn vị thời gian
+                    currentProcess.RemainingTime--;
+                    currentTime++;
+
+                    // Cập nhật thời gian chờ cho các tiến trình khác
+                    foreach (var process in readyProcesses)
+                    {
+                        if (process != currentProcess)
+                        {
+                            process.WaitingTime++;
+                        }
+                    }
+
+                    // Nếu tiến trình hoàn thành
+                    if (currentProcess.RemainingTime == 0)
+                    {
+                        currentProcess.CompletionTime = currentTime;
+                        completedProcesses.Add(currentProcess);
+                    }
+                }
+                else
+                {
+                    currentTime++; // Nếu không có tiến trình nào đến, tăng thời gian
+                }
+            }
+
+            // Cập nhật DataGridView với kết quả
+            for (int i = 0; i < completedProcesses.Count; i++)
+            {
+                var process = completedProcesses[i];
+                int rowIndex = processes.FindIndex(p => p.ProcessID == process.ProcessID);
+                dgv_process.Rows[rowIndex].Cells["WaitingTime"].Value = process.WaitingTime;
+                dgv_process.Rows[rowIndex].Cells["CompletionTime"].Value = process.CompletionTime;
+            }
+
+            // Tính thời gian chờ trung bình
+            double averageWaitingTime = completedProcesses.Average(p => p.WaitingTime);
+            MessageBox.Show("Thời gian chờ trung bình: " + averageWaitingTime);
+        }
+    }
+}
